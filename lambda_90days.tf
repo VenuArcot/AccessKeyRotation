7cbd266f45dd2a198287e6f6a28f57374f3ec09d
EC2 Default User <ec2-user@ip-172-31-61-50.ec2.internal>
2020-12-26 21:49:19 +0000

    wip

--- lambda_90days.tf
+++ lambda_90days.tf
@@ -1,10 +1,73 @@
+resource "aws_iam_policy" "policy" {
+  name        = "inline_lambda_policy"
+  description = "A policy granting resource access to AccessKeyRotation lambda functions"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "secretsmanager:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "iam:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "accesskeyrotation_lambda_role" {
+
+  assume_role_policy = <<EOF
+{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Effect": "Allow"
+        }
+      ]
+    }
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test_attach" {
+  role       = aws_iam_role.accesskeyrotation_lambda_role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+data "archive_file" "lambda90days" {
+  type        = "zip"
+  source_dir = "lambda_90days"
+  output_path = "lambda90days.zip"
+}
+
 module "lamdba_function" {
-  source = "terraform-aws-modules/lambda/aws"
+  source        = "github.com/terraform-module/terraform-aws-lambda?ref=v2.9.0"
   function_name = "accesskeyrotation_90days"
-  handler = "index.lambda_handler"
-  runtime = "python3.7"
-  source_path="lambda_90days"
+  handler       = "lambda90days.lambda_handler"
+  runtime       = "python3.7"
+  filename      = "lambda90days.zip"
+  source_code_hash = data.archive_file.lambda90days.output_base64sha256
+  description   = ""
+  memory_size    = "128"
+  concurrency    = "5"
+  lambda_timeout = "20"
+  log_retention  = "1"
+  role_arn      = aws_iam_role.accesskeyrotation_lambda_role.arn
   tags = {
-    Name = "accesskeyrotation_90days"
+    name = "accesskeyrotation_90days"
   }
 }
--- lambda_90days/lambda90days.py
+++ lambda_90days/lambda90days.py
@@ -3,30 +3,30 @@ import boto3
 import sys
 from botocore.exceptions import ClientError
 
-gpg = gnupg.GPG('/usr/bin/gpg')
+def lambda_handler(event, context):
+    gpg = gnupg.GPG('/usr/bin/gpg')
 
-secrets_client = boto3.client(
-    service_name='secretsmanager',
-    region_name='us-east-1'
-)
-iam_client = boto3.client(
-     service_name='iam',
-     region_name='us-east-1'
-)
+    secrets_client = boto3.client(
+        service_name='secretsmanager',
+        region_name='us-east-1'
+    )
+    iam_client = boto3.client(
+        service_name='iam',
+        region_name='us-east-1'
+    )
 
-user_name = sys.argv[1]
+    user_name = event['username'] 
 
-user = iam_client.get_user(
-    UserName=user_name
-)
+    user = iam_client.get_user(
+        UserName=user_name
+    )
 
-access_key = iam_client.create_access_key(
-    UserName=user_name
-)
-access_key_json='{"new_access_key": "%s", "new_secret_access_key": "%s"}'%(access_key['AccessKey']['AccessKeyId'],access_key['AccessKey']['SecretAccessKey'])
-print(access_key_json)
-secrets_client.put_secret_value(
-    SecretId=user_name,
-    SecretString=access_key_json
-)
+    access_key = iam_client.create_access_key(
+        UserName=user_name
+    )
+    access_key_json='{"new_access_key": "%s", "new_secret_access_key": "%s"}'%(access_key['AccessKey']['AccessKeyId'],access_key['AccessKey']['SecretAccessKey'])
+    secrets_client.put_secret_value(
+        SecretId=user_name,
+        SecretString=access_key_json
+    )
 
