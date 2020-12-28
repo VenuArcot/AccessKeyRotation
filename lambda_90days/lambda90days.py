0a840911239676a4a2a51b0b7f0e0abe0f58b95f
EC2 Default User <ec2-user@ip-172-31-61-50.ec2.internal>
2020-12-28 03:49:58 +0000

    wip

--- lambda_90days/lambda90days.py
+++ lambda_90days/lambda90days.py
@@ -4,29 +4,32 @@ import sys
 from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
-    gpg = gnupg.GPG('/usr/bin/gpg')
+    try:
+        gpg = gnupg.GPG('/usr/bin/gpg')
 
-    secrets_client = boto3.client(
-        service_name='secretsmanager',
-        region_name='us-east-1'
-    )
-    iam_client = boto3.client(
-        service_name='iam',
-        region_name='us-east-1'
-    )
+        secrets_client = boto3.client(
+            service_name='secretsmanager',
+            region_name='us-east-1'
+        )
+        iam_client = boto3.client(
+            service_name='iam',
+            region_name='us-east-1'
+        )
 
-    user_name = event['username'] 
+        user_name = event['username'] 
 
-    user = iam_client.get_user(
-        UserName=user_name
-    )
+        user = iam_client.get_user(
+            UserName=user_name
+        )
 
-    access_key = iam_client.create_access_key(
-        UserName=user_name
-    )
-    access_key_json='{"new_access_key": "%s", "new_secret_access_key": "%s"}'%(access_key['AccessKey']['AccessKeyId'],access_key['AccessKey']['SecretAccessKey'])
-    secrets_client.put_secret_value(
-        SecretId=user_name,
-        SecretString=access_key_json
-    )
-
+        access_key = iam_client.create_access_key(
+            UserName=user_name
+        )
+        access_key_json='{"new_access_key": "%s", "new_secret_access_key": "%s"}'%(access_key['AccessKey']['AccessKeyId'],access_key['AccessKey']['SecretAccessKey'])
+        secrets_client.put_secret_value(
+            SecretId=user_name,
+            SecretString=access_key_json
+        )
+        return "email"
+    except:
+        return "error"
